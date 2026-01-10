351b89cbe9bd18d28344172b1c57ae59ca6b9969
AkaiVAC <41547081+AkaiVAC@users.noreply.github.com>
2026-01-10 23:34:29 +0530

    refactor(extension-system): remove auto-discovery

--- src/extension-system/index.ts
+++ src/extension-system/index.ts
@@ -1,20 +1,2 @@
-export type { DiscoveredExtension } from "./1-discover/discover-extensions";
-export { discoverExtensions } from "./1-discover/discover-extensions";
-
-export type Extension = {
-    name: string;
-    version: string;
-    description?: string;
-    author?: string;
-    dependencies?: string[];
-    provides?: {
-        loaders?: string[];
-        validators?: string[];
-        contextBuilders?: string[];
-        senders?: string[];
-        uiComponents?: string[];
-    };
-    activate: () => void | Promise<void>;
-};
-
-export const defineExtension = (ext: Extension): Extension => ext;
+export type { ExtensionEntry, ExtensionsConfig, Status } from "./types";
+export { loadConfig } from "./config-loader";
